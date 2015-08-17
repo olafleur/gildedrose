6e2cb1211bc7ad964b71657f08164eb5a07ca13c
Olivier Lafleur <olivier.lafleur@gmail.com>
2015-08-17 00:24:51 -0400

    elixir + fix

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -19,15 +19,29 @@ public class GildedRoseTest {
 	public void dexterityVest() {
         list.add(new Item("+5 Dexterity Vest", 10, 20));
 
-        assertEquals(9, GildedRose.updateQuality(list).get(0).getSellIn());
-        assertEquals(18, GildedRose.updateQuality(list).get(0).getQuality());
+        List<Item> resultingList = GildedRose.updateQuality(list);
+
+        assertEquals(9, resultingList.get(0).getSellIn());
+        assertEquals(19, resultingList.get(0).getQuality());
 	}
 
     @Test
     public void agedBrie() {
         list.add(new Item("Aged Brie", 2, 0));
 
-        assertEquals(1, GildedRose.updateQuality(list).get(0).getSellIn());
-        assertEquals(2, GildedRose.updateQuality(list).get(0).getQuality());
+        List<Item> resultingList = GildedRose.updateQuality(list);
+
+        assertEquals(1, resultingList.get(0).getSellIn());
+        assertEquals(1, resultingList.get(0).getQuality());
+    }
+
+    @Test
+    public void elixir() {
+        list.add(new Item("Elixir of the Mongoose", 5, 7));
+
+        List<Item> resultingList = GildedRose.updateQuality(list);
+
+        assertEquals(4, resultingList.get(0).getSellIn());
+        assertEquals(6, resultingList.get(0).getQuality());
     }
 }
