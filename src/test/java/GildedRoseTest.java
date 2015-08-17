d69b9d72de8f80ff173d580bff629b1e7df83316
Olivier Lafleur <olivier.lafleur@gmail.com>
2015-08-17 00:18:12 -0400

    Second test

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -7,15 +8,26 @@ import java.util.List;
 
 
 public class GildedRoseTest {
+    private List<Item> list;
+
+    @Before
+    public void before() {
+        list = new ArrayList<Item>();
+    }
 
 	@Test
-	public void dexterityVestDecreasesTo9and18() {
-		//Given
-        List<Item> list = new ArrayList<Item>();
-
+	public void dexterityVest() {
         list.add(new Item("+5 Dexterity Vest", 10, 20));
 
         assertEquals(9, GildedRose.updateQuality(list).get(0).getSellIn());
         assertEquals(18, GildedRose.updateQuality(list).get(0).getQuality());
 	}
+
+    @Test
+    public void agedBrie() {
+        list.add(new Item("Aged Brie", 2, 0));
+
+        assertEquals(1, GildedRose.updateQuality(list).get(0).getSellIn());
+        assertEquals(2, GildedRose.updateQuality(list).get(0).getQuality());
+    }
 }
